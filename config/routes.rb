3e8c28c7180e5f5f2b991b26a65cd07029ebf291
likelion-joon13 <racoon0705@gmali.com>
2018-04-14 09:30:40 +0000

    eunjin

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,22 @@ Rails.application.routes.draw do
   # get '/developer' => 'home#info'
   # get '/contact' => 'home#contact'
   get '/' => 'home#sorry'
+  # 자기소개 페이지 
+  # 1. 인건
   get '/inkun', :to => redirect('/inkun.html')
+  # 2. 구자룡
   get '/kuku', :to => redirect('/kuja.html')
+  # 3. 김기중
   get '/kijung', :to => redirect('/ki_jung.html')
+  # 4. 박상원
   get '/sangwon', :to => redirect('/sangwon.html')
+  # 5. 신승윤
   get '/seungyun', :to => redirect('/seungyun.html')
+  # 6. 함석원
   get '/seokwon', :to => redirect('/Hamseokwon.html')
+  # 7. 박현우
   get '/hyunwoo', :to => redirect('/Hyunwoo.html')
-  # 태훈 라우트 
+  # 8. 이태훈 
   get '/about_T1', :to => redirect('/about_T1.html')
   get '/contact_T1', :to => redirect('/contact_T1.html')
   get '/favorites_T1', :to => redirect('/favorites_T1.html')
@@ -21,7 +29,14 @@ Rails.application.routes.draw do
   get '/Teuno', :to => redirect('/Teuno.html')
   get '/pixelart_T1', :to => redirect('/pixelart_T1.html')
   get '/trailers_T1', :to => redirect('/trailers_T1.html')
+  # 9. 이슬
   get '/leeseul', :to => redirect('/leeseul.html')
+  # 10. 김은진
+  get '/eunjin', :to => redirect('/index_eunjin.html')
+  # 11.
+  get '/', :to => redirect('/.html')
+  
+  # 라우트 리다이렉트 
   get '*path' => redirect('/sorry') 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
